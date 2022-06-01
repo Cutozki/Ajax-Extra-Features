22afe6c736cef12936fc37beafd588bbcfacd76b
Cutozki <98546755+Cutozki@users.noreply.github.com>
2022-06-01 12:24:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -105,7 +105,7 @@ async def start(client, message):
         await asyncio.sleep(2)
         await m.delete()
         await message.reply_photo(
-            photo=random.choice(PICS),
+            video=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
